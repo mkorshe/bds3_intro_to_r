fd0bf933f4f403b65a677fbf94fd05be3c7294f0
mkorshe <korshe.mar@gmail.com>
2023-07-08 23:54:32 +0300

    Update intro_to_r.R
    adding dplyr and vis parts

--- intro_to_r.R
+++ intro_to_r.R
@@ -70,7 +70,7 @@ lengths
 ??geom_point
 
 
-##### Робота з даними ####
+##### Робота з даними - завантаження і читання ####
 
 download.file("https://raw.githubusercontent.com/datacarpentry/R-genomics/gh-pages/data/Ecoli_metadata.csv", "Ecoli_metadata.csv")
 
@@ -95,4 +95,173 @@ str(metadata)
 # Що таке клас метаданих об’єкта?
 # Скільки рядків і скільки стовпців у цьому об’єкті?
 # Скільки цитрат+ мутантів було зареєстровано в цій популяції? citrate+ mutants
+
+
+##### Робота з даними - data frame  ####
+
+ten_letters <- c('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
+ten_letters[2]
+ten_letters[c(1,7)]
+
+ten_letters[3:6]
+
+ten_letters[10:1]
+
+ten_letters[c(2, 8:10)]
+
+#
+
+seq(1, 10, by = 2)
+seq(20, 4, by = -3)
+
+#
+
+metadata[1, 2]   # 1st element in the 2nd column 
+metadata[1, 6]   # 1st element in the 6th column
+metadata[1:3, 7] # First three elements in the 7th column
+metadata[3, ]    # 3rd element for all columns
+metadata[, 7]    # Entire 7th column
+
+# The function nrow() on a data.frame returns the number of rows. For example, try typing nrow(metadata). Usenrow()andseq()to create a new data frame calledmeta_by_2that includes all even numbered rows ofmetadata`.
+# Select the strain column from metadata
+metadata[ , "strain"]
+
+# Alternatively...
+metadata$strain
+
+metadata[, c("strain", "clade")]
+
+metadata[4:7, c("strain", "clade")]
+
+##### dplyr ####
+
+install.packages("dplyr") ## install
+library("dplyr")          ## load
+select(metadata, sample, clade, cit, genome_size)
+filter(metadata, cit == "plus")
+
+
+metadata %>%
+  filter(cit == "plus") %>%
+  select(sample, generation, clade)
+
+meta_citplus <- metadata %>%
+  filter(cit == "plus") %>%
+  select(sample, generation, clade)
+
+meta_citplus
+
+# Challenge
+# Using pipes, subset the data to include rows where the clade is ‘Cit+’. Retain columns sample, cit, and genome_size.
+
+
+metadata %>%
+  mutate(genome_bp = genome_size *1e6)
+
+metadata %>%
+  mutate(genome_bp = genome_size *1e6) %>%
+  head
+
+metadata %>%
+  mutate(genome_bp = genome_size *1e6) %>%
+  filter(!is.na(clade)) %>%
+  head
+
+
+#
+
+metadata %>%
+  group_by(cit) %>%
+  summarize(n())
+
+#
+
+metadata %>%
+  group_by(cit) %>%
+  summarize(mean_size = mean(genome_size, na.rm = TRUE))
+
+metadata %>%
+  group_by(cit, clade) %>%
+  summarize(mean_size = mean(genome_size, na.rm = TRUE))
+
+metadata %>%
+  group_by(cit, clade) %>%
+  summarize(mean_size = mean(genome_size, na.rm = TRUE)) %>%
+  filter(!is.na(clade))
+
+
+metadata %>%
+  group_by(cit, clade) %>%
+  summarize(mean_size = mean(genome_size, na.rm = TRUE),
+            min_generation = min(generation))
+
+##### data visualization ####
+
+genome_size <- metadata$genome_size
+
+
+plot(genome_size)
+
+plot(genome_size, pch=8)
+
+plot(genome_size, pch=8, main="Scatter plot of genome sizes")
+
+hist(genome_size)
+
+boxplot(genome_size ~ cit, metadata)
+
+boxplot(genome_size ~ cit, metadata,  col=c("pink","purple", "darkgrey"),
+        main="Average expression differences between celltypes", ylab="Expression")
+
+#### Advances visualization ####
+
+library(ggplot2)
+
+ggplot(metadata) +
+  geom_point() 
+
+
+ggplot(metadata) +
+  geom_point(aes(x = sample, y= genome_size))
+
+ggplot(metadata) +
+  geom_point(aes(x = sample, y= genome_size, color = generation, shape = cit), size = rel(3.0)) +
+  theme(axis.text.x = element_text(angle=45, hjust=1))
+
+ggplot(metadata) +
+  geom_bar(aes(x = genome_size))
+
+
+ggplot(metadata) +
+  geom_bar(aes(x = genome_size), stat = "bin", binwidth=0.05)
+
+
+ggplot(metadata) +
+  geom_boxplot(aes(x = cit, y = genome_size, fill = cit)) +
+  ggtitle('Boxplot of genome size by citrate mutant type') +
+  xlab('citrate mutant') +
+  ylab('genome size') +
+  theme(panel.grid.major = element_line(size = .5, color = "grey"),
+        axis.text.x = element_text(angle=45, hjust=1),
+        axis.title = element_text(size = rel(1.5)),
+        axis.text = element_text(size = rel(1.25)))
+
+pdf("boxplot.pdf")
+
+ggplot(example_data) +
+  geom_boxplot(aes(x = cit, y =....) +
+                 ggtitle(...) +
+                 xlab(...) +
+                 ylab(...) +
+                 theme(panel.grid.major = element_line(...),
+                       axis.text.x = element_text(...),
+                       axis.title = element_text(...),
+                       axis.text = element_text(...)
+                       
+                       dev.off()
+                       
+                       
+
+
+
 
